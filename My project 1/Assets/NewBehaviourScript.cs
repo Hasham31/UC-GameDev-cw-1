ec14e8ff266ab128cf5849b15a59bdc7cca86ab0
Hasham31 <142688132+Hasham31@users.noreply.github.com>
2023-08-23 20:07:05 +0300

    ZERO

---
+++ My project 1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    string heroname = "zero";
+    string villainame = " The  shadow ";
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(heroname);
+        print(villainame);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
